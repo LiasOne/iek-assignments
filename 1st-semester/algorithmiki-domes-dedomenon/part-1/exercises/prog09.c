f4030f91c346f97975eebb8e98fb8cc9ec2f0481
Eno <eno@vostro.localdomain>
2018-11-18 23:58:58 +0200

    prog09

---
+++ 1st-semester/algorithmiki-domes-dedomenon/part-1/exercises/prog09.c
@@ -0,0 +1,24 @@
+//  prog09.c – τυπώνει τα μεγέθη των τύπων δεδομένων
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+  float waight;
+  int size, letters;
+  char name[40];
+
+  printf("Γεια σου! Πώς λέγεσαι;\n");
+
+  scanf("%s", name);
+  printf("Ποιο είναι το βάρος σου σε κιλά %s;\n", name);
+
+  scanf("%f", &waight);
+
+  size = sizeof(name);
+  letters = strlen(name);
+
+  printf("Γεια σου, %s, το βάρος σου είναι %2.2f κιλά.\n", name, waight);
+  printf("Επίσης, το όνομα σου έχει %d γράμματα,\n", letters);
+  printf("και χρειάζονται %d bytes για την αποθήκευση του.\n", size);
+}
