511beab1197b79655df334a1b907131b3da63b29
Eno <eno@vostro.localdomain>
2018-11-23 17:40:08 +0200

    Last com

--- 1st-semester/algorithmiki-domes-dedomenon/part-1/exercises/prog03.c
+++ 1st-semester/algorithmiki-domes-dedomenon/part-1/exercises/prog03.c
@@ -3,19 +3,28 @@
 #include <stdio.h>
 
 void florina();
+int prosthesi();
 
 int main()
 {
-  //
   printf("Θα καλέσω τη συνάρτηση της Φλώρινας.\n" );
   florina();
 
-  //
   printf("Η κλήση της συνάρτησης έγινε.\n");
+  prosthesi();
 }
 
 void florina()
 {
-  //
   printf("Γεια σας από Φλώρινα\n");
+}
+
+int prosthesi()
+{
+  int a, b;
+  a = 5;
+  b = 7;
+
+  printf("%d + %d = %d \n", a, b, a + b);
+return 0;
 }
