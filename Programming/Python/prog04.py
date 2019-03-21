c576e0f4cc75cd22bf48c18ed8b456e5be245484
Eno <eno@vostro.localdomain>
2019-03-21 17:22:46 +0200

    21/03/19

---
+++ Programming/Python/prog04.py
@@ -0,0 +1,15 @@
+shoppinglist = [ "Cheese", "Rice", "Coffee", "Milk", "Camba" ]
+for element in shoppinglist:
+    print (element)
+
+element = raw_input ("what are you buing today? ")
+
+if element in shoppinglist:
+    print ("Yes, this is on the list")
+else:
+    print ("Not on the list, you don't need it")
+
+for i in [1,2,3,4,5,6,7,8,9,10]:
+    print (i)
+for i in range (1,1001):
+    print (i)
