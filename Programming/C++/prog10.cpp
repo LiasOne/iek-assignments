a18864bffad9e6cae0a629617b61288691c4b774
Eno <eno@vostro.localdomain>
2019-03-27 17:35:06 +0200

    27/03/19

---
+++ Programming/C++/prog10.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+void readMatrix (int array [3][3], int nRaw, int nCol) {
+  cout << "Δώσε τιμές στον πίνακα: " << endl;
+  for (int i = 0; i < nRaw; i++) {
+    for (int j = 0; j < nCol; j++)
+      cout << array [i][j] << " ";
+    cout << endl;
+  }
+
+  cout << "Ο πίνακας είναι: " << endl;
+  for (int i = 0; i < nRaw; i++) {
+    for (int j = 0; j < nCol; j++)
+      cout << array [i][j] << " ";
+    cout << endl;
+  }
+}
+
+void newArray (int array [3][3], int array2 [3][3], int nRaw, int nCol) {
+  for (int i = 0; i < nRaw; i++)
+    for (int j = 0; j < nCol; j++) {
+      if (array [i][j] == 0)
+        array2 [i][j] = 0;
+      else if (array [i][j] < 0)
+        array2 [i][j] = -1;
+      else if (array [i][j] > 0)
+        array2 [i][j] = 1;
+    }
+
+  cout << "Ο νέος πίνακας είναι: " << endl;
+  for (int i = 0; i < nRaw; i++) {
+    for (int j = 0; j < nCol; j++)
+      cout << array2 [i][j] << " ";
+    cout << endl;
+  }
+}
+
+int main() {
+  int array [3][3];
+  int array2 [3][3];
+
+  readMatrix (array, 3,3);
+  newArray (array, array2, 3,3);
+  return 0;
+}
